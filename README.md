35caca1386d28910a5b0fc73a213597c5bd7b6cd
Hailey Flo <hkflo@email.unc.edu>
2021-11-14 14:07:20 -0500

    progress

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # MadLibs
 Hack110 project
+
+Our project is a remade version of mad libs. It consists of three simulation types: the basic, multiple-choice, and story versions. In basic, the format is the same as the traditional madlibs. In multiple choice, it's slightly easier because the user doesn't have to come up with their own answers. Story is the hardest version because it has multiple layers and multiple story outcomes.
+
+Comp 110 programming concepts used:
+    1. calling outside files/websites
+    2. calling main
+    3. declaring variables
+
+Challenges overcome:
+- Declaring so many labels
+- Accidentally repeating a word that broke our entire code
+
+New concepts learned:
+- Declaring labels and how to translate them
+- how to import an image and insert it
+- GET and POST
+
+Team members and contributions of each member:
+Stella Hileman:
+    Wrote all labels. Wrote app requests. Wrote mad libs.
+Hailey Flo:
+    Wrote and implemented CSS. Set up website and inserted the code. Created and edited files.
+
+Goals Achieved:
+    We made a successful mad libs program that did what we wanted it to do. All of our files work correctly and have no errors.
+
+Goals Outstanding: 
+    We weren't able to use all of our stories. We should go back and make more possible madlibs (generated stories) for the multiple choice and basic categories.
--- app.py
+++ app.py
@@ -21,7 +21,8 @@ def multi_quiz():
       
         if family_member_1 == '' or adjective_1 == '' or clothing_item_1 == '' or adjective_2 == '' or clothing_item_2 == '' or location_1 == '' or location_2 == '' or room_1 == '':
             return render_template("multi_quiz.html")
-        return render_template("multi_result.html")
+        
+        return render_template("multi_result.html", family_member_1=family_member_1, adjective_1=adjective_1, clothing_item_1=clothing_item_1, adjective_2=adjective_2, clothing_item_2=clothing_item_2, location_1=location_1, location_2=location_2, room_1=room_1)
     return render_template("multi_quiz.html")
 
 
@@ -47,6 +48,127 @@ def basic_quiz():
            return render_template("basic_quiz.html")
        return render_template("basic_result.html", holiday_1=holiday_1, noun_1=noun_1, name_1=name_1, noun_2=noun_2, plural_noun_1=plural_noun_1, verb_1=verb_1, adjective_1=adjective_1, name_2=name_2, noun_3=noun_3, adjective_2=adjective_2, name_3=name_3, past_tense_verb_1=past_tense_verb_1, past_tense_verb_2=past_tense_verb_2)
     return render_template("basic_quiz.html")
+
+@app.route('/story_scene', methods=["GET", "POST"])
+def story_scene():
+    if request.method == "POST":
+        celebrity_1: str = request.form['celebrity_1']
+        location_1: str = request.form['location_1']
+        adjective_1: str = request.form['adjective_1']
+        noun_1: str = request.form['noun_1']
+        if celebrity_1 == '' or location_1 == '' or adjective_1 == '' or noun_1 == '':
+            return render_template("story_scene.html")
+        return render_template("story_scene_result.html", celebrity_1=celebrity_1, location_1=location_1, adjective_1=adjective_1, noun_1=noun_1)
+    return render_template("story_scene.html")
+ 
+
+@app.route('/story_scene_result', methods=["GET", "POST"])
+def story_scene_result():
+    if request.method == "POST":
+        choice_1: str = request.form['choice_1']
+        if choice_1 == 'run':
+            return render_template("story_run.html")
+        return render_template("story_arrested.html")
+    return render_template("story_scene_result.html")
+
+ 
+ 
+@app.route('/story_run', methods=["GET", "POST"])
+def story_run():
+    if request.method == "POST":
+        animal_1: str = request.form['animal_1']
+        color_1: str = request.form['color_1']
+        color_2: str = request.form['color_2']
+        sound_effect_plural_1: str = request.form['sound_effect_plural_1']
+        if animal_1 == '' or color_1 == '' or color_2 == '' or sound_effect_plural_1 == '':
+            return render_template("story_run.html")
+        return render_template("story_run_result.html", animal_1=animal_1, color_1=color_1, color_2=color_2, sound_effect_plural_1=sound_effect_plural_1)
+    return render_template("story_run.html")
+ 
+ 
+@app.route('/story_run_result', methods=["GET", "POST"])
+def story_run_result():
+    if request.method == "POST":
+        choice_run: str = request.form['choice_run']
+        if choice_run == 'hide':
+            return render_template("story_hide.html")
+        if choice_run == 'keep':
+            return render_template("story_keep.html")
+    return render_template("story_run_result.html")
+ 
+
+@app.route('/story_hide', methods=["GET", "POST"])
+def story_hide():
+    if request.method == "POST":
+        plural_body_part_1: str = request.form['plural_body_part_1']
+        verb_1: str = request.form['verb_1']
+        time_1: str = request.form['time_1']
+        facial_feature_1: str = request.form['facial_feature_1']
+        if plural_body_part_1 == '' or verb_1 == '' or time_1 == '' or facial_feature_1 == '':
+            return render_template("story_hide.html")
+        return render_template("story_hide_result.html", plural_body_part_1=plural_body_part_1, verb_1=verb_1, time_1=time_1, facial_feature_1=facial_feature_1)
+    return render_template("story_hide.html")
+ 
+ 
+@app.route('/story_keep', methods=["GET", "POST"])
+def story_keep():
+    if request.method == "POST":
+        verb_1: str = request.form['verb_1']
+        verb_2: str = request.form['verb_2']
+        noun_1: str = request.form['noun_1']
+        if verb_1 == '' or verb_2 == '' or noun_1 == '':
+            return render_template("story_keep.html")
+        return render_template("story_keep_result.html", verb_1=verb_1, verb_2=verb_2, noun_1=noun_1)
+    return render_template("story_keep.html")
+ 
+ 
+@app.route('/story_arrested', methods=["GET", "POST"])
+def story_arrested():
+    if request.method == "POST":
+        adjective_1: str = request.form['adjective_1']
+        adjective_2: str = request.form['adjective_2']
+        verb_1: str = request.form['verb_1']
+        if adjective_1 == '' or adjective_2 == '' or verb_1 == '':
+            return render_template("story_arrested.html")
+        return render_template("story_arrested_result.html", adjective_1=adjective_1, adjective_2=adjective_2, verb_1=verb_1)
+    return render_template("story_arrested.html")
+ 
+ 
+@app.route('/story_arrested_result', methods=["GET", "POST"])
+def story_arrested_result():
+    if request.method == "POST":
+        choice_arrested: str = request.form['choice_arrested']
+        if choice_arrested == '':
+            return render_template("story_arrested_result.html")
+        if choice_arrested == 'guilty':
+            return render_template("story_guilty.html")
+        if choice_arrested == 'insanity':
+            return render_template("story_insanity.html")
+    return render_template("story_arrested_result.html")
+ 
+ 
+@app.route('/story_guilty', methods=["GET", "POST"])
+def story_guilty():
+    if request.method == "POST":
+        location_1: str = request.form['location_1']
+        time_1: str = request.form['time_1']
+        question_1: str = request.form['question_1']
+        if location_1 == '' or time_1 == '' or question_1 == '':
+            return render_template("story_guilty.html")
+        return render_template("story_guilty_result.html", location_1=location_1, time_1=time_1, question_1=question_1)
+    return render_template("story_guilty.html")
+ 
+ 
+@app.route('/story_insanity', methods=["GET", "POST"])
+def story_insanity():
+    if request.method == "POST":
+        past_verb_1: str = request.form['past_verb_1']
+        noun_1: str = request.form['noun_1']
+        if past_verb_1 == '' or noun_1 == '':
+            return render_template("story_insanity.html")
+        return render_template("story_insanity_result.html", past_verb_1=past_verb_1, noun_1=noun_1)
+    return render_template("story_insanity.html")
+ 
 
 if __name__ == '__main__':
     app.run(debug=True)
